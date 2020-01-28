70192d0ce4b28bbd76276392190998cbb9a7b60c
Austin <austin@8thlight.com>
2020-01-27 16:16:40 -0800

    add code to pass first test

--- resistor-color-duo.js
+++ resistor-color-duo.js
@@ -1,2 +1,5 @@
-export const decodedValue = () => {
+export const decodedValue = (firstColor, secondColor) => {
+  return parseInt(`${COLORS.indexOf(firstColor)}${COLORS.indexOf(secondColor)}`)
 };
+
+const COLORS = ["Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey", "White"]
--- resistor-color-duo.spec.js
+++ resistor-color-duo.spec.js
@@ -1,7 +1,7 @@
 import { decodedValue } from './resistor-color-duo.js'
 
 describe("decodedValue", () => {
-  test("it should return 00 when invoked with 'Brown' and 'Black'", () => {
+  test("it should return 10 when invoked with 'Brown' and 'Black'", () => {
     expect(decodedValue("Brown", "Black")).toEqual(10)
   })
 })
