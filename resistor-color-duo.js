a922ad5dabe0ed7c043776a0ef7b22aafdcc6ed4
Austin <austin@8thlight.com>
2020-01-27 16:35:52 -0800

    alter tests to reflect the input of arrays as intended

--- resistor-color-duo.js
+++ resistor-color-duo.js
@@ -1,4 +1,4 @@
-export const decodedValue = (firstColor, secondColor) => {
+export const decodedValue = ([firstColor, secondColor]) => {
   return parseInt(`${COLORS.indexOf(firstColor)}${COLORS.indexOf(secondColor)}`)
 };
 
