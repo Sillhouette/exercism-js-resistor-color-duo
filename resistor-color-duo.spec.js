7751a3725d11ebdebb429eb913cbaa054e3bc850
Austin <austin@8thlight.com>
2020-01-27 16:19:19 -0800

    add two more tests

--- resistor-color-duo.spec.js
+++ resistor-color-duo.spec.js
@@ -8,4 +8,12 @@ describe("decodedValue", () => {
   test("it should return 19 when invoked with 'Brown' and 'White'", () => {
     expect(decodedValue("Brown", "White")).toEqual(19)
   })
+
+  test("it should return 35 when invoked with 'Orange' and 'Green'", () => {
+    expect(decodedValue("Orange", "Green")).toEqual(35)
+  })
+
+  test("it should return 27 when invoked with 'Red' and 'Violet'", () => {
+    expect(decodedValue("Red", "Violet")).toEqual(27)
+  })
 })
