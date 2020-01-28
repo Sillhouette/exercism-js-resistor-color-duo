0e2cf15127c4a4d2f512175453d4d84aac4396bf
Austin <austin@8thlight.com>
2020-01-27 16:38:49 -0800

    refactor to use lowercased args

--- resistor-color-duo.js
+++ resistor-color-duo.js
@@ -2,4 +2,4 @@ export const decodedValue = ([firstColor, secondColor]) => {
   return parseInt(`${COLORS.indexOf(firstColor)}${COLORS.indexOf(secondColor)}`)
 };
 
-const COLORS = ["Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey", "White"]
+const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
--- resistor-color-duo.spec.js
+++ resistor-color-duo.spec.js
@@ -2,26 +2,26 @@ import { decodedValue } from './resistor-color-duo.js'
 
 describe("decodedValue", () => {
   test("it should return 10 when invoked with 'Brown' and 'Black'", () => {
-    expect(decodedValue(["Brown", "Black"])).toEqual(10)
+    expect(decodedValue(["brown", "black"])).toEqual(10)
   })
 
   test("it should return 89 when invoked with 'Grey' and 'White'", () => {
-    expect(decodedValue(["Grey", "White"])).toEqual(89)
+    expect(decodedValue(["grey", "white"])).toEqual(89)
   })
 
   test("it should return 35 when invoked with 'Orange' and 'Green'", () => {
-    expect(decodedValue(["Orange", "Green"])).toEqual(35)
+    expect(decodedValue(["orange", "green"])).toEqual(35)
   })
 
   test("it should return 27 when invoked with 'Red' and 'Violet'", () => {
-    expect(decodedValue(["Red", "Violet"])).toEqual(27)
+    expect(decodedValue(["red", "violet"])).toEqual(27)
   })
 
   test("it should return 46 when invoked with 'Yellow' and 'Blue'", () => {
-    expect(decodedValue(["Yellow", "Blue"])).toEqual(46)
+    expect(decodedValue(["yellow", "blue"])).toEqual(46)
   })
 
   test("it should return 46 when invoked with 'Yellow', 'Blue', and 'Red'", () => {
-    expect(decodedValue(["Yellow", "Blue", "Red"])).toEqual(46)
+    expect(decodedValue(["yellow", "blue", "red"])).toEqual(46)
   })
 })
