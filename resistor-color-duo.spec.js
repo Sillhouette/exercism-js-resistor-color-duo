e19ed5fe6067b9cbc36c72b908fd9f11f09406a0
Austin <austin@8thlight.com>
2020-01-27 16:47:56 -0800

    refactor tests to use "give, act, expect" pattern

--- resistor-color-duo.spec.js
+++ resistor-color-duo.spec.js
@@ -2,26 +2,38 @@ import { decodedValue } from './resistor-color-duo.js'
 
 describe("decodedValue", () => {
   test("it should return 10 when invoked with 'Brown' and 'Black'", () => {
-    expect(decodedValue(["brown", "black"])).toEqual(10)
+    const given = ["brown", "black"]
+    const action = decodedValue(given)
+    expect(action).toEqual(10)
   })
 
   test("it should return 89 when invoked with 'Grey' and 'White'", () => {
-    expect(decodedValue(["grey", "white"])).toEqual(89)
+    const given = ["grey", "white"]
+    const action = decodedValue(given)
+    expect(action).toEqual(89)
   })
 
   test("it should return 35 when invoked with 'Orange' and 'Green'", () => {
-    expect(decodedValue(["orange", "green"])).toEqual(35)
+    const given = ["orange", "green"]
+    const action = decodedValue(given)
+    expect(action).toEqual(35)
   })
 
   test("it should return 27 when invoked with 'Red' and 'Violet'", () => {
-    expect(decodedValue(["red", "violet"])).toEqual(27)
+    const given = ["red", "violet"]
+    const action = decodedValue(given)
+    expect(action).toEqual(27)
   })
 
   test("it should return 46 when invoked with 'Yellow' and 'Blue'", () => {
-    expect(decodedValue(["yellow", "blue"])).toEqual(46)
+    const given = ["yellow", "blue"]
+    const action = decodedValue(given)
+    expect(action).toEqual(46)
   })
 
   test("it should return 46 when invoked with 'Yellow', 'Blue', and 'Red'", () => {
-    expect(decodedValue(["yellow", "blue", "red"])).toEqual(46)
+    const given = ["yellow", "blue", "red"]
+    const action = decodedValue(given)
+    expect(action).toEqual(46)
   })
 })
